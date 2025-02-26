0cfc3cc54b8ffcc0433075a528059949d31a30f3
hth810 <3430426834@qq.com>
2025-02-26 19:44:12 +0800

    cross origin

--- src/main/java/com/school/config/WebConfig.java
+++ src/main/java/com/school/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.school.config;
 import com.school.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //登录，注册不拦截
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:3000") // 允许的域名
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                .allowedHeaders("*") // 允许的头信息
+                .allowCredentials(true); // 允许携带cookie
     }
 }
